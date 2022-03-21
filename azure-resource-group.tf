1cb865076c0f4c14c52feab1c5d2f6628d34eed9
gopal das <gopal1409@gmail.com>
2022-03-21 20:10:08 +0530

    terraform resource group creation

---
+++ azure-resource-group.tf
@@ -0,0 +1,25 @@
+#terrform setting block
+#this terraform setting block change whenever you change your cloud provider. 
+terraform {
+    required_providers {
+        azurerm = {
+            source = "hashicorp/azurerm" #it will go to registry.terraform.io and download all the plugin needed to communicate with azure.
+            version = "=2.97.0"
+        }
+    }
+}
+
+
+
+#configure the microsoft azure provider
+provider "azurerm"{
+
+}
+
+
+#create resource group
+
+resource "azurerm_resource_group" "my_demo_rg1" {
+  location = "eastus"
+  name = "my-demo-rg1"
+}
